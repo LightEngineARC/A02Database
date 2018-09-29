4b7a9ac4da8ef8e8b3f3bb8263982f2fac49b726
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-29 03:21:49 -0600

    begin creating more statments for filtering songs

--- src/database/SongsArtistsAlbums.java
+++ src/database/SongsArtistsAlbums.java
@@ -1,30 +1,39 @@
 /**
- *  @author Ashton Chatelain
- *  
- *  A02Database
- *  Description : Queries to be used to sort table data based on choices on the GUI
+ * @author Ashton Chatelain
+ * <p>
+ * A02Database
+ * Description : Queries to be used to sort table data based on choices on the GUI
  */
 package database;
 
 /**
  * @author Ashton Chatelain
- *
  */
-public class SongsArtistsAlbums
-{
+public class SongsArtistsAlbums {
 
-	    // - - - - - - - - - - - query statements - - - - - - - - - - - - -
+    // - - - - - - - - - - - query statements - - - - - - - - - - - - -
 
-		/**
-		 * Query all data from songs and add artists and albums when found
-		 *
-		 */
-	    public static String query_All() {
-	        return "SELECT SONGS.TITLE, ARTISTS.ARTIST_NAME as ARTIST, ALBUMS.ALBUM_NAME as ALBUM "
-	        		+ "FROM SONGS "
-	        		+ "Left join artists on artists.artist_id = songs.artist_id "
-	        		+ "left join albums on albums.album_id = songs.album_id "
-	        		+ "order by songs.title ASC";
-	    }
+    /**
+     * Query all data from songs and add artists and albums when found
+     */
+    public static String query_All() {
+        return "SELECT SONGS.TITLE, ARTISTS.ARTIST_NAME as ARTIST, ALBUMS.ALBUM_NAME as ALBUM "
+                + "FROM SONGS "
+                + "Left join artists on artists.artist_id = songs.artist_id "
+                + "left join albums on albums.album_id = songs.album_id "
+                + "order by songs.title ASC";
+    }
+
+    /**
+     * Query all data from songs and add artists and albums when found
+     */
+    public static String query_Song_String(String s) {
+        return "SELECT SONGS.TITLE, ARTISTS.ARTIST_NAME as ARTIST, ALBUMS.ALBUM_NAME as ALBUM "
+                + "FROM SONGS "
+                + "WHERE SONGS.TITLE LIKE '" + s + "' "
+                + "Left join artists on artists.artist_id = songs.artist_id "
+                + "left join albums on albums.album_id = songs.album_id "
+                + "order by songs.title ASC";
+    }
 
 }

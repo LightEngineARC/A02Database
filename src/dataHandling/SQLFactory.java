71263efe43008b6557f5d33db8ae887712cefdcb
A-L-M <al3cmills@gmail.com>
2018-10-03 14:28:48 -0600

    add associative parser generation

--- src/dataHandling/SQLFactory.java
+++ src/dataHandling/SQLFactory.java
@@ -4,9 +4,9 @@ public class SQLFactory {
     public static void main(String[] args) {
         Parser[] parsers = new Parser[]{
                 new ArtistsParser(), new SongsParser(), new AlbumsParser(), new AlbumVersionsParser(),
-//                new AssociativeParser.ArtistsAlbumsParser(),
-//                new AssociativeParser.ArtistsSongsParser(),
-//                new AssociativeParser.SongsAlbumsParser(),
+                new AssociativeParser.ArtistsAlbumsParser(),
+                new AssociativeParser.ArtistsSongsParser(),
+                new AssociativeParser.SongsAlbumsParser(),
         };
 
         for (Parser parser : parsers) {

b3c450e0570ffe6a1a7a05e0924f398c756f246b
A-L-M <al3cmills@gmail.com>
2018-10-08 14:11:13 -0600

    add create view

--- src/dataHandling/Test.java
+++ src/dataHandling/Test.java
@@ -15,12 +15,14 @@ public class Test {
 //                "LEFT JOIN album_versions ON album_versions.id = l_songs_album_versions.album_version " +
 //                "LEFT JOIN albums ON albums.id = album_versions.album " +
 //                "WHERE albums.name NOT LIKE '%null%'");
-            db.executeQueries("SELECT DISTINCT artists.name, album_versions.name, songs.name FROM album_versions " +
-                "INNER JOIN l_artists_album_versions ON l_artists_album_versions.album_version = album_versions.id " +
-                "INNER JOIN artists ON artists.id = l_artists_album_versions.artist " +
-                "INNER JOIN l_artists_songs ON l_artists_songs.artist = artists.id " +
-                "INNER JOIN songs ON songs.id = l_artists_songs.song " +
-                "ORDER BY artists.name");
+            db.executeSqlStatement("CREATE VIEW all_content (artist, album, song) AS " +
+                    "SELECT DISTINCT artists.name, album_versions.name, songs.name FROM album_versions " +
+                    "INNER JOIN l_artists_album_versions ON l_artists_album_versions.album_version = album_versions.id " +
+                    "INNER JOIN artists ON artists.id = l_artists_album_versions.artist " +
+                    "INNER JOIN l_artists_songs ON l_artists_songs.artist = artists.id " +
+                    "INNER JOIN songs ON songs.id = l_artists_songs.song " +
+                    "ORDER BY artists.name");
+            db.executeQueries("SELECT * FROM all_content FETCH FIRST 1000 ROWS ONLY");
         } catch (SQLException e) {
             e.printStackTrace();
         }
